b6c873fa43956cd88be474769d8f5005400c47a9
Step_atlas <stp.sip7@gmail.com>
2023-02-27 09:44:16 -0500

    title

--- src/components/common/footer/Footer.tsx
+++ src/components/common/footer/Footer.tsx
@@ -1,7 +1,9 @@
 import { FC } from "react";
 
 const Footer: FC = () => {
-  return <div>this is our footer</div>;
+  return (
+    <div className="flex items-center justify-center">Have a nice day!</div>
+  );
 };
 
 export default Footer;
